f1ee6172573fa754d345bd8dbe34069a3c23d6a4
Jasmina Dang <jasminadang@gmail.com>
2021-09-20 21:59:48 +1000

    created the frontend for the food home page

---
+++ SoftwareStudio3/SoftwareStudio3/FoodPage.swift
@@ -0,0 +1,31 @@
+//
+//  FoodPage.swift
+//  SoftwareStudio3
+//
+//  Created by Jasmina Dang on 20/9/21.
+//
+
+import UIKit
+
+class FoodPage: UIViewController {
+
+    @IBOutlet var addFood: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
