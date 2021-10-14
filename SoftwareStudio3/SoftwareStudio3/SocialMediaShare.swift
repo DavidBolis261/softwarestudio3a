6f595380465abde6cc5b6e4f881aed4d5eb5e2be
Amaan Ansari <amaan_ans@hotmail.com>
2021-10-14 15:11:30 +1100

    social media sharesheet
    basic implementation of the social media share sheet

---
+++ SoftwareStudio3/SoftwareStudio3/SocialMediaShare.swift
@@ -0,0 +1,21 @@
+//
+//  SocialMediaShare.swift
+//  SoftwareStudio3
+//
+//  Created by Amaan Ansari on 14/10/21.
+//
+
+import Foundation
+import UIKit
+
+class viewController: UIViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func PresentShareSheet() {
+    }
+    
+}
+
