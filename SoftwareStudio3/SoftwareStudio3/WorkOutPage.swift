3ea532724dbb20ada7b7278d5db482459c6a8c72
David Bolis <50950141+DavidBolis261@users.noreply.github.com>
2021-10-21 14:26:30 +1100

    More UI Maintenance and Changes

--- SoftwareStudio3/SoftwareStudio3/WorkOutPage.swift
+++ SoftwareStudio3/SoftwareStudio3/WorkOutPage.swift
@@ -6,24 +6,27 @@
 //
 
 import UIKit
+import Foundation
+import CoreMotion
 
 class WorkOutPage: UIViewController {
     var workoutTypeStr = ""
     var imagetoDisplaystr = ""
-    
-    
-    
-    
-    
+    var OurTimer = Timer()
+    var TimerDisplayed = 3
+    var Reps = 0
+    @IBOutlet weak var CounterText: UILabel!
     @IBOutlet weak var workoutTypeOut: UILabel!
     @IBOutlet weak var workoutImageOut: UIImageView!
     @IBOutlet weak var caloriesNumOut: UILabel!
     @IBOutlet weak var calculatedTime: UILabel!
     @IBOutlet weak var excerciseQTYOut: UILabel!
     @IBOutlet weak var ActivityBtnOutlet: UIButton!
-   
     @IBOutlet weak var theinsightViewOut: UIView!
     @IBOutlet weak var GrayedWallOut: UIView!
+    var motion = CMMotionManager()
+    
+    @IBOutlet weak var numberOfReps: UILabel!
     var activityStarted  = false
     
     override func viewDidLoad() {
@@ -35,8 +38,8 @@ class WorkOutPage: UIViewController {
     }
     
     @IBAction func activityBtnAction(_ sender: Any) {
-        activityStarted.toggle()
-        
+       
+        OurTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerDown), userInfo: nil, repeats: true)
         if activityStarted{
             GrayedWallOut.isHidden = true
             ActivityBtnOutlet.setTitle("Stop Now", for: .normal)
@@ -48,6 +51,41 @@ class WorkOutPage: UIViewController {
         }
     }
     
-
-
+    @objc func TimerDown(){
+        TimerDisplayed -= 1
+        if (TimerDisplayed == 0){
+            CounterText.text = "GO!"
+            GrayedWallOut.isHidden = true
+            CounterText.isHidden = true
+            OurTimer.invalidate()
+            myAccel()
+            
+        } else {
+            CounterText.text = "\(TimerDisplayed)"
+        }
+    }
+    func MyGyro(){
+        motion.gyroUpdateInterval = 0.1
+        motion.startGyroUpdates(to: OperationQueue.current!){(data, error) in
+            if let trueData = data{
+                print(trueData)
+            }
+        }
+    }
+    func myAccel(){
+        motion.gyroUpdateInterval = 0.1
+        motion.startAccelerometerUpdates(to: OperationQueue.current!){(data, error) in
+            if let trueData = data{
+                var accelX = trueData.acceleration.x
+                var accelY = trueData.acceleration.y
+                var accelZ = trueData.acceleration.z
+                var Total = accelX + accelY + accelZ
+                if (Total > 8){
+                    self.Reps += 1
+                    print(Total)
+                    self.numberOfReps.text = "\(self.Reps)"
+                }
+            }
+        }
+    }
 }
